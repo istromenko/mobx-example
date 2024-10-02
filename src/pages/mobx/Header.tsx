27886f2dd1119cf687b35b6a75c5a7d7d257fc13
IlyaStromenko <iistromenkowork@gmail.com>
2024-10-02 16:03:57 +0300

    code stylize

--- src/pages/mobx/Header.tsx
+++ src/pages/mobx/Header.tsx
@@ -1,23 +1,26 @@
-import { observer } from "mobx-react";
-import nameStore from "./store.ts";
 import React from 'react';
+import { observer } from 'mobx-react';
+import nameStore from './store.ts';
 
-@observer class Header extends React.Component {
+@observer
+class Header extends React.Component {
+    handleNameChange = (event) => {
+        nameStore.setName(event.target.value);
+    };
+
     render() {
-        const handleChange = (event) => {
-            nameStore.changeName(event.target.value);
-        };
         return (
             <header style={{display: 'flex', justifyContent: 'center'}}>
                 <input
                     type="text"
                     value={nameStore.name}
-                    onChange={handleChange}
+                    onChange={this.handleNameChange}
                     placeholder="Enter your name"
                     style={{padding: '10px', fontSize: '16px'}}
                 />
             </header>
-        )
+        );
     }
-};
+}
+
 export default Header;
