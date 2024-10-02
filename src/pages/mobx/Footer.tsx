ed9fde7fe1643552f5e7601e0237043e9033dd15
IlyaStromenko <iistromenkowork@gmail.com>
2024-10-02 15:34:01 +0300

    work version

--- src/pages/mobx/Footer.tsx
+++ src/pages/mobx/Footer.tsx
@@ -1,12 +1,14 @@
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 import nameStore from "./store.ts";
+import React from 'react';
 
-const Footer = observer(() => {
-    return (
-        <footer style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-            <label style={{ fontSize: '16px' }}>Name: {nameStore.name}</label>
-        </footer>
-    );
-});
-
+@observer class Footer extends React.Component {
+    render() {
+        return (
+            <footer style={{display: 'flex', justifyContent: 'center', padding: '20px'}}>
+                <label style={{fontSize: '16px'}}>Name: {nameStore.name}</label>
+            </footer>
+        );
+    }
+};
 export default Footer;
--- src/pages/mobx/Header.tsx
+++ src/pages/mobx/Header.tsx
@@ -1,22 +1,23 @@
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 import nameStore from "./store.ts";
+import React from 'react';
 
-const Header= observer(() => {
-    const handleChange = (event) => {
-        nameStore.changeName(event.target.value);
-    };
-
-    return (
-        <header style={{ display: 'flex', justifyContent: 'center' }}>
-            <input
-                type="text"
-                value={nameStore.name}
-                onChange={handleChange}
-                placeholder="Enter your name"
-                style={{ padding: '10px', fontSize: '16px' }}
-            />
-        </header>
-    );
-});
-
+@observer class Header extends React.Component {
+    render() {
+        const handleChange = (event) => {
+            nameStore.changeName(event.target.value);
+        };
+        return (
+            <header style={{display: 'flex', justifyContent: 'center'}}>
+                <input
+                    type="text"
+                    value={nameStore.name}
+                    onChange={handleChange}
+                    placeholder="Enter your name"
+                    style={{padding: '10px', fontSize: '16px'}}
+                />
+            </header>
+        )
+    }
+};
 export default Header;
